0a99e1ad1d1d406e46ba647b8980cba21bc48b3b
danrus1100 <danrus228zk@gmail.com>
2025-06-04 19:31:24 +0300

    ModConfigScreen literals fix

--- src/main/java/net/chibify/chibify/config/ModConfigScreen.java
+++ src/main/java/net/chibify/chibify/config/ModConfigScreen.java
@@ -5,28 +5,22 @@ import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
-import java.lang.reflect.Field;
-import java.net.URI;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ModConfigScreen {
     public static Screen create(Screen parent) {
         return YetAnotherConfigLib.createBuilder()
-                .title(Text.of("Chibify"))
+                .title(Text.translatable("chibify.config.title"))
                 .category(ConfigCategory.createBuilder()
-                        .name(Text.of("General"))
+                        .name(Text.translatable("chibify.config.category.general"))
                         .option(Option.<Boolean>createBuilder()
-                                .name(Text.of("Self-Chibi"))
-                                .description(OptionDescription.of(Text.of("Render yourself as chibi")))
+                                .name(Text.translatable("chibify.config.general.self-chibi"))
+                                .description(OptionDescription.of(Text.translatable("chibify.config.general.self-chibi.description")))
                                 .binding(true, () -> ModConfig.INSTANCE.shrinkSelf, newValue -> ModConfig.INSTANCE.shrinkSelf = newValue)
                                 .controller(BooleanControllerBuilder::create)
                                 .build())
 
                         .option(Option.<Boolean>createBuilder()
-                                .name(Text.of("AccurateEyePos"))
-                                .description(OptionDescription.of(Text.of("Lower the eye pos to be more accurate when shrunk")))
+                                .name(Text.translatable("chibify.config.general.accurate_eye_pos"))
+                                .description(OptionDescription.of(Text.translatable("chibify.config.general.accurate_eye_pos.description")))
                                 .binding(true, () -> ModConfig.INSTANCE.AccurateEyeHeight, newValue -> ModConfig.INSTANCE.AccurateEyeHeight = newValue)
                                 .controller(BooleanControllerBuilder::create)
                                 .build())
